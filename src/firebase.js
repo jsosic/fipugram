ec1dc855d6da6047d49dcc2fb19c74714267a193
Nikola Tanković <nikola.tankovic@gmail.com>
2021-01-05 17:26:30 +0100

    firebase modul

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: 'AIzaSyAkj47q4t1djLkdQfmSGC2XOSCzs52oSo4',
+    authDomain: 'fipugram.firebaseapp.com',
+    projectId: 'fipugram',
+    storageBucket: 'fipugram.appspot.com',
+    messagingSenderId: '1003961861611',
+    appId: '1:1003961861611:web:fba9233c32e8a61b8905ae',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+export { firebase, db };
