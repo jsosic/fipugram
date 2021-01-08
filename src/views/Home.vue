383ca72f392c35ee16df092cf80cfd44c8d7e0ab
Nikola Tanković <nikola.tankovic@gmail.com>
2021-01-08 14:03:05 +0100

    Dodavanje novog posta

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
     <div class="row">
         <div class="col-8">
+            <form @submit.prevent="postNewImage" class="form-inline mb-5">
+                <div class="form-group">
+                    <label for="imageUrl">Image URL</label>
+                    <input
+                        v-model="newImageUrl"
+                        type="text"
+                        class="form-control ml-2"
+                        placeholder="Enter the image URL"
+                        id="imageUrl"
+                    />
+                </div>
+                <div class="form-group">
+                    <label for="imageDescription">Description</label>
+                    <input
+                        v-model="newImageDescription"
+                        type="text"
+                        class="form-control ml-2"
+                        placeholder="Enter the image description"
+                        id="imageDescription"
+                    />
+                </div>
+                <button type="submit" class="btn btn-primary ml-2">Post image</button>
+            </form>
             <instagram-card v-for="card in filteredCards" :key="card.url" :info="card" />
         </div>
         <div class="col-4">
@@ -12,6 +35,7 @@
 // @ is an alias to /src
 import InstagramCard from '@/components/InstagramCard.vue';
 import store from '@/store';
+import { db } from '@/firebase';
 
 let cards = [];
 
@@ -29,7 +53,31 @@ export default {
         return {
             cards,
             store,
+            newImageDescription: '',
+            newImageUrl: '',
         };
+    },
+    methods: {
+        postNewImage() {
+            const imageUrl = this.newImageUrl;
+            const imageDescription = this.newImageDescription;
+
+            db.collection('posts')
+                .add({
+                    url: imageUrl,
+                    desc: imageDescription,
+                    email: store.currentUser,
+                    posted_at: Date.now(),
+                })
+                .then((doc) => {
+                    console.log('Spremljeno', doc);
+                    this.newImageDescription = '';
+                    this.newImageUrl = '';
+                })
+                .catch((e) => {
+                    console.error(e);
+                });
+        },
     },
     computed: {
         filteredCards() {
